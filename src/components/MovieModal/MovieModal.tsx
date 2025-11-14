ad7b5e406c3ffdfa5625ec697381c0a75e9fc15d
pona <ipona3710@gmail.com>
2025-11-14 14:19:19 +0200

    update MoveModal.tsx

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,5 +1,5 @@
 import { createPortal } from "react-dom";
-import { useEffect, type MouseEvent  } from "react";
+import { useEffect, type MouseEvent } from "react";
 import type { Movie } from "../../types/movie";
 import css from "./MovieModal.module.css";
 
@@ -10,8 +10,8 @@ interface ModalProps {
 
 export default function Modal({ movie, onClose }: ModalProps) {
   const { title, overview, release_date, vote_average, poster_path } = movie;
-  
-    useEffect(() => {
+
+  useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.code === "Escape") {
         onClose();
@@ -25,28 +25,43 @@ export default function Modal({ movie, onClose }: ModalProps) {
     };
   }, [onClose]);
 
-    const handleBackdropClick = (e: MouseEvent<HTMLDivElement>) => {
-      if (e.target === e.currentTarget) {
-        onClose();
-      }
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "";
     };
+  }, []);
 
-    return createPortal(
+  const handleBackdropClick = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return createPortal(
     <div className={css.backdrop} onClick={handleBackdropClick}>
-          <div className={css.modal} onClick={e => e.stopPropagation()}>
-          <button type="button" className={css.closeButton} onClick={onClose} aria-label="Close modal">
-            ×
-          </button>
-              <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} className={css.image} alt={movie.title} />
-              <div className={css.content}>
-                  <h2>{title}</h2>
-                  <p>{overview}</p>
-                  <p>Release date: {release_date}</p>
-                  <p>Vote average: {vote_average}</p>
-              </div>
-          </div>
+      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
+        <button
+          type="button"
+          className={css.closeButton}
+          onClick={onClose}
+          aria-label="Close modal"
+        >
+          ×
+        </button>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+          className={css.image}
+          alt={movie.title}
+        />
+        <div className={css.content}>
+          <h2>{title}</h2>
+          <p>{overview}</p>
+          <p>Release date: {release_date}</p>
+          <p>Vote average: {vote_average}</p>
+        </div>
+      </div>
     </div>,
     document.body
   );
 }
-
